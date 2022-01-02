74acd4671003f63384de9f620e5ce1778f0ef5fc
Felix Adam <26323807+fadamDS@users.noreply.github.com>
2022-01-02 16:05:00 +0100

    Test for correlation function

---
+++ tests/test_eval.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from src.evaluation import corr_score
+
+
+# Constant Variables
+data_path = "data/gresearch/"
+
+
+def test_corr_score():
+
+    # Public LB min date
+    min_date = pd.to_datetime('2021-06-13 00:00:00')
+
+    train = pd.read_csv(data_path + 'raw/train.csv')
+    train['timestamp'] = pd.to_datetime(train.timestamp, unit='s')
+    asset_info = pd.read_csv(data_path + 'raw/asset_details.csv')
+
+    test_df = train[train.timestamp >= min_date].copy()
+
+    # Naive predictions
+    test_df['naive_prediction'] = np.log(test_df.Close / test_df.Open)
+    test_df = (test_df.merge(asset_info[['Asset_ID', 'Weight']],
+                             on='Asset_ID', how='left')
+               .dropna(subset=['Target', 'naive_prediction']))
+
+    score = corr_score(test_df.Target,
+                       test_df.naive_prediction,
+                       test_df.Weight)
+
+    assert(score == -0.008372884483520249)
