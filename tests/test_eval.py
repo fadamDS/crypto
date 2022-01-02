489dd0be60225137995260e0edccad838cc316d3
Felix Adam <26323807+fadamDS@users.noreply.github.com>
2022-01-02 21:27:02 +0100

    test for target recreation

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from src.evaluation import corr_score
+from src.utils import recreate_gresearch_target
 
 
 # Constants
@@ -8,9 +9,11 @@ data_path = "data/gresearch/"
 train = pd.read_csv(data_path + 'raw/train.csv')
 train['timestamp'] = pd.to_datetime(train.timestamp, unit='s')
 asset_info = pd.read_csv(data_path + 'raw/asset_details.csv')
+train = train.merge(asset_info[['Asset_ID', 'Weight']],
+                    on='Asset_ID', how='left')
 
 
-def test_corr_score(train,asset_info):
+def test_corr_score():
 
     # Public LB min date
     min_date = pd.to_datetime('2021-06-13 00:00:00')
@@ -19,12 +22,27 @@ def test_corr_score(train,asset_info):
 
     # Naive predictions
     test_df['naive_prediction'] = np.log(test_df.Close / test_df.Open)
-    test_df = (test_df.merge(asset_info[['Asset_ID', 'Weight']],
-                             on='Asset_ID', how='left')
-               .dropna(subset=['Target', 'naive_prediction']))
+    test_df = (test_df.dropna(subset=['Target', 'naive_prediction']))
 
     score = corr_score(test_df.Target,
                        test_df.naive_prediction,
                        test_df.Weight)
 
     assert(score == -0.008372884483520249)
+
+
+def test_recreate_gresearch_target():
+
+    df = recreate_gresearch_target(data=train, asset_info=asset_info,
+                                   price_column='Close')
+
+    # NaN's match between Target and recon_Target?
+    assert(all(df.Target.isna() == df.recon_Target.isna()))
+
+    # None of the targets dropped
+    assert(len(df.Target) == len(df.Target))
+
+    abserror = abs(df['Target'] - df['recon_Target']).dropna()
+
+    # Erros sufficiently small
+    np.all(abserror < 1e-13)
