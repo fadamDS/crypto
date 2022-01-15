19a6f402456c568989928ccc209c0166cddcf1f8
Felix Adam <26323807+fadamDS@users.noreply.github.com>
2022-01-15 20:31:55 +0100

    rolling features in engineer all features

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 relative_cols = ['Count', 'Open',
                  'High', 'Low', 'Close',
                  'Volume', 'VWAP']
@@ -11,3 +13,18 @@ lagged_cols = ['direct_return', 'log_return', 'high_low_ratio',
                'log_change_Volume_1min', 'log_change_VWAP_1min']
 
 lagged_periods = [1, 2, 3, 4, 5]
+
+non_train_cols = ['Target', 'Weight', 'timestamp',
+                  'Asset_ID', 'Count', 'Open',
+                  'High', 'Low', 'Close',
+                  'Volume', 'VWAP']
+
+rolling_cols = ['log_return']
+
+rolling_periods = [15, 30, 90, 180]
+
+max_lookback_minutes = np.max(
+                              np.concatenate((lagged_periods,
+                                              relative_periods,
+                                              rolling_periods))
+                              )
--- src/utils.py
+++ src/utils.py
@@ -11,8 +11,6 @@ klines_colnames = ['openTime', 'open', 'high',
 
 assets = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-non_train_cols = ['Target', 'Weight', 'timestamp', 'Asset_ID']
-
 
 def load_gresearch_raw(data_path):
 
