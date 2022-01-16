553d74cbf27c29abe3572ac9305fe8657a9ea121
Felix Adam <26323807+fadamDS@users.noreply.github.com>
2022-01-16 17:21:52 +0100

    dart base model

--- src/models.py
+++ src/models.py
@@ -1,36 +1,139 @@
 import numpy as np
 import pandas as pd
+import lightgbm as lgb
+import os
+from scipy import stats
+from src.settings import assets, dart_base_params, weights, names
+from src.evaluation import corr_score
 
 
-class BaseCryptoLearner():
+class CryptoDART():
 
-    def __init__(self, assets=None):
-        self.initialized = True
-        self.learned_mean = {}
+    def __init__(self,
+                 assets: list = assets,
+                 weights: dict = weights,
+                 names: dict = names,
+                 params: dict = dart_base_params,
+                 ):
         self.assets = assets
+        self.weights = weights
+        self.names = names
+        self.params = params
+        self.models = {}
 
-    def train(self, data):
-        # Just take the target median over the sample for each coin
-        for asset in self.assets:
-            mean = np.mean(data[data.Asset_ID == asset].Target)
-            if np.isnan(mean):
-                mean = 0
-            self.learned_mean[asset] = mean
+    def make_data(self,
+                  features: list,
+                  target: str,
+                  train: pd.DataFrame,
+                  test: pd.DataFrame = None):
+        """
+        Training and testing lgb data for each coin
+        """
+        self.features = features
+        self.target = target
+        self.data = {}
 
-    def predict(self, X):
-        # Should always be with (AssetID & Timestamp)
-        # Predict the learned mean
-        self.predictions = []
+        for asset_id in self.assets:
+            print(f'Making data for {asset_id}')
 
-        for asset in self.assets:
+            asset_train = train[train.Asset_ID == asset_id]
+            asset_lgb_train = lgb.Dataset(data=asset_train[features],
+                                          label=asset_train[target],
+                                          feature_name=features,)
+            self.data[asset_id] = {'lgb_train': asset_lgb_train}
 
-            pred = pd.DataFrame({'Asset_ID': asset,
-                                 'timestamp': X[X.Asset_ID == asset].timestamp})
+            if test is not None:
+                self.test_available = True
+                asset_test = test[test.Asset_ID == asset_id]
 
-            pred['prediction'] = np.repeat(
-                self.learned_mean[asset], repeats=pred.shape[0])
-            self.predictions.append(pred)
+                asset_lgb_test = lgb.Dataset(data=asset_test[features],
+                                             label=asset_test[target])
 
-        self.predictions_df = pd.concat(self.predictions)
+                self.data[asset_id]['lgb_test'] = asset_lgb_test
 
-        return self.predictions_df
+        print('Data creation done')
+        print('------------\n')
+
+    def train(self):
+        """
+        Train models for each coin
+        """
+
+        for asset_id in self.assets:
+
+            print(f'Training {asset_id}')
+
+            lgb_train = self.data[asset_id]['lgb_train']
+
+            dart = lgb.train(params=self.params,
+                             train_set=lgb_train)
+
+            self.models[asset_id] = dart
+            print('------------\n')
+
+        print(f'Training for {asset_id} done')
+        print('------------\n')
+
+    def run_full_test(self):
+
+        if self.test_available:
+            print('Running test')
+            results_list = []
+            for asset_id in self.assets:
+
+                lgb_test = self.data[asset_id]['lgb_test']
+                dart = self.models[asset_id]
+                predictions = dart.predict(lgb_test.data)
+
+                result = pd.DataFrame(
+                    {'prediction': predictions, 'target': lgb_test.label})
+                result['Asset_ID'] = asset_id
+                result['Weight'] = self.weights[asset_id]
+                results_list.append(result)
+
+            self.results_df = pd.concat(results_list)
+
+            self.test_score = corr_score(self.results_df.target,
+                                         self.results_df.prediction,
+                                         self.results_df.Weight)
+        else:
+            print('No test data available')
+
+    def save_models(self,
+                    save_dir: str,
+                    base_name: str):
+
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
+        for asset_id in self.assets:
+
+            dart = self.models[asset_id]
+            name = self.names[asset_id]
+
+            save_path = save_dir + base_name + name + '.txt'
+            dart.save_model(save_path)
+
+    def save_test_results(self, save_path):
+
+        pearsons = []
+        perason_p_vals = []
+        mae = []
+        names = []
+        for asset_id in self.assets:
+
+            result_data = self.results_df[self.results_df.Asset_ID == asset_id]
+            pearson_res = stats.pearsonr(
+                result_data.target, result_data.prediction)
+            pearsons.append(pearson_res[0])
+            perason_p_vals.append(pearson_res[1])
+            mae.append(
+                np.mean(np.abs(result_data.prediction - result_data.target)))
+            names.append(self.names[asset_id])
+
+        pd.DataFrame({'full_score': self.test_score,
+                      'asset_id': self.assets,
+                      'asset_name': names,
+                      'pearson': pearsons,
+                      'pearson_pval': perason_p_vals,
+                      'mae': mae}).to_csv(save_path)
